9cf509f6e5ecb37bed3d9c395bcfdfb1002a65ad
ridipblog <memorytemp5@gmail.com>
2024-04-17 15:36:41 +0530

    add footer with responsive

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -29,9 +29,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-
-    <!-- Template Stylesheet -->
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/ridip_media.css') }}">
 </head>
 
 <body>
@@ -115,7 +115,8 @@
                                 <div class="col-md-3">
                                     <div class="date" id="date2" data-target-input="nearest">
                                         <input type="text" class="form-control datetimepicker-input"
-                                            placeholder="Check out" data-target="#date2" data-toggle="datetimepicker" />
+                                            placeholder="Check out" data-target="#date2"
+                                            data-toggle="datetimepicker" />
                                     </div>
                                 </div>
                                 <div class="col-md-3">
@@ -231,29 +232,6 @@
         <!-- About End -->
 
 
-        <!-- Newsletter Start -->
-        <div class="container newsletter mt-5 wow fadeIn" data-wow-delay="0.1s">
-            <div class="row justify-content-center">
-                <div class="col-lg-10 border rounded p-1">
-                    <div class="border rounded text-center p-1">
-                        <div class="bg-white rounded text-center p-5">
-                            <h4 class="mb-4">Subscribe Our <span
-                                    class="text-primary text-uppercase">Newsletter</span>
-                            </h4>
-                            <div class="position-relative mx-auto" style="max-width: 400px;">
-                                <input class="form-control w-100 py-3 ps-4 pe-5" type="text"
-                                    placeholder="Enter your email">
-                                <button type="button"
-                                    class="btn btn-primary py-2 px-3 position-absolute top-0 end-0 mt-2 me-2">Submit</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Newsletter Start -->
-
-
         <!-- Footer Start -->
         {{-- <div class="container-fluid bg-dark text-light footer">
             <div class="container">
@@ -322,21 +300,23 @@
             </div>
         </div> --}}
 
-
+        <x-footer-component></x-footer-component>
 
 
         <!-- Footer End -->
 
 
         <!-- Back to Top -->
-        <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+        {{-- <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a> --}}
     </div>
 
     <!-- JavaScript Libraries -->
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
+    <script src="https://kit.fontawesome.com/42bb61fedf.js" crossorigin="anonymous"></script>
 
 
 
--- resources/views/components/footer-component.blade.php
+++ resources/views/components/footer-component.blade.php
@@ -1,3 +1,47 @@
-<div class="">
-
+<div class="container-fluid d-flex flex-wrap col-12  main-footer-div">
+    <div class="d-flex flex-wrap col-12  social-footer-div">
+        <div class="d-flex flex-wrap col-md-4 col-11 social-footer-div-1">
+            <p>US ON SOCIAL MEDIA....</p>
+        </div>
+        <div class="d-flex flex-wrap col-md-7 col-11 social-footer-div-1">
+            <a class="d-flex " href=""><i class="fab fa-twitter"></i></a>
+            <a class="d-flex" href=""><i class="fab fa-facebook-f"></i></a>
+            <a class="d-flex" href=""><i class="fab fa-youtube"></i></a>
+            <a class="d-flex" href=""><i class="fab fa-linkedin-in"></i></a>
+        </div>
+    </div>
+    <div class="d-flex flex-wrap col-12 content-footer-div">
+        <div class="d-flex flex-wrap col-md-3 col-5 content-footer-div-1">
+            <p>Daavilla</p>
+            <p>Daavilla is in one of the trendiest sections of Guwahati, placing close to railway station, Airport,
+                business district and popular attractions. </p>
+        </div>
+        <div class="d-flex flex-wrap col-md-3 col-5 content-footer-div-1">
+            <p>contact</p>
+            <p class=""><i class="fa fa-map-marker-alt me-2"></i>Kahilipara, Guwahati Assam</p>
+            <p class=""><i class="fa fa-phone-alt me-2"></i>+012 345 67890</p>
+            <p class=""><i class="fa fa-envelope me-2"></i>info@example.com</p>
+        </div>
+        <div class="d-flex flex-wrap col-md-2 col-5 content-footer-div-1">
+            <p>about us</p>
+            <a class="" href=""><span><i class="fa-solid fa-caret-right me-2"></i></span>Contact Us</a>
+            <a class="" href=""><span><i class="fa-solid fa-caret-right me-2"></i></span>Privacy Policy</a>
+            <a class="" href=""><span><i class="fa-solid fa-caret-right me-2"></i></span>Terms &
+                Condition</a>
+            <a class="" href=""><span><i class="fa-solid fa-caret-right me-2"></i></span>Support</a>
+        </div>
+        <div class="d-flex flex-wrap col-md-2 col-5 content-footer-div-1">
+            <p>Services</p>
+            <a class="" href=""><span><i class="fa-solid fa-caret-right me-2"></i></span>Food &
+                Restaurant</a>
+            <a class="" href=""><span><i class="fa-solid fa-caret-right me-2"></i></span>Spa &
+                Fitness</a>
+            <a class="" href=""><span><i class="fa-solid fa-caret-right me-2"></i></span>Sports &
+                Gaming</a>
+            <a class="" href=""><span><i class="fa-solid fa-caret-right me-2"></i></span>Event &
+                Party</a>
+            <a class="" href=""><span><i class="fa-solid fa-caret-right me-2"></i></span>GYM &
+                Yoga</a>
+        </div>
+    </div>
 </div>
