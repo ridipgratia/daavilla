db60ba98cde24cb9574eea670ca7cf5751d86f61
ridipblog <memorytemp5@gmail.com>
2024-04-17 10:49:29 +0530

    add route file

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+// ----------------------- ridip routes ---------------------
+require __DIR__ . '/ridip.php';
+// ----------------------- dipankor routes ---------------------
+
+require __DIR__ . '/dipankor.php';
