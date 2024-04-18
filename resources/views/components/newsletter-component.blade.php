c81f05f37773cc116b0ab7a7fe32cb31960f1872
ridipblog <memorytemp5@gmail.com>
2024-04-18 13:08:52 +0530

    add newsletter in about page

---
+++ resources/views/components/newsletter-component.blade.php
@@ -0,0 +1,30 @@
+<div class="container d-flex flex-wrap col-12 main-newsletter-div">
+    <button class="newsletter-btn"><i class="fa-solid fa-envelope"></i></button>
+    <div class="d-flex flex-wrap col-md-4 col-12 newsletter-form-div">
+        <div class="d-flex flex-wrap col-11 newsletter-head">
+            <span class="col-2 newsletter-close"><i class="fa-solid fa-arrow-left"></i></span>
+            <span class="col-10">NEWSLETTER</span>
+        </div>
+        <div class="d-flex flex-wrap col-11 type-newsletter">
+            <p class="col-12">Type of send notification</p>
+            <div class="d-flex flex-wrap col-12 gap-2 type-newsletter-1">
+                <input type="checkbox" id="all_news">
+                <span>All type of notification</span>
+            </div>
+            <div class="d-flex flex-wrap col-12 gap-2 type-newsletter-1">
+                <input type="checkbox" id="room_news">
+                <span>Send rooms related notification</span>
+            </div>
+            <div class="d-flex flex-wrap col-12 gap-2 type-newsletter-1">
+                <input type="checkbox" id="price_news">
+                <span>Send price related notification</span>
+            </div>
+        </div>
+        <div class="d-flex col-11 email-newsletter">
+            <span class="col-1"><i class="fa-solid fa-envelope"></i></span>
+            <input type="email" id="email_news" class="col-11">
+        </div>
+        <button class="col-11">SUBMIT</button>
+    </div>
+
+</div>
