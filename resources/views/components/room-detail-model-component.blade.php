c2411bda2d2507b1167b29f6d14176cae86d3ad9
dipankor99 <dipankor.gratia@gmail.com>
2024-04-20 10:34:35 +0530

    add home content

--- resources/views/components/room-detail-model-component.blade.php
+++ resources/views/components/room-detail-model-component.blade.php
@@ -1,3 +1,11 @@
+<style>
+    .feature{
+        color: #4A3B31;
+    }
+    .feature:hover{
+        text-decoration: underline;
+    }
+</style>
 <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
     <div class="modal-dialog modal-xl modal-dialog-centered modal-dialog-scrollable">
         <div class="modal-content">
@@ -32,34 +40,31 @@
                     </div>
                 </div>
                 <div class="col-md-6">
-
-                    <p class="text-danger fw-bold">Rooms: <span class=""> 201 202 203 204</span> <span class="bg-danger text-white rounded py-1 px-3 ms-4"> Estimated Price Rs. 4000 </span> </p> 
+                    <p class="text-danger fw-bold">Rooms: <span class=""> 201 | 202 | 203 | 204</span> <span class="bg-danger text-white rounded py-1 px-3 ms-4"> Estimated Price Rs. 4000 </span> </p> 
                     <p class="text-body mb-3">Experience luxury in our Junior Suite Rooms, featuring a pool view, king bed, sofa, and more. Enjoy amenities like a mini bar, bathrobe, and entertainment options.</p>
-
                         <br> 
                         <h3>Room Features</h3>
                         <p>Room Size: 
                             <div class="row">
                                 <div class="col">
-                                    <li>Garden /pool view</li> 
-                                    <li>Entertainment</li> 
-                                    <li>WIFI</li> 
-                                    <li>A/C</li> 
-                                    <li>King Bed</li> 
-                                    <li>Couch sofa with coffee table</li> 
-                                    <li>Television</li> 
+                                    <li class="feature">Garden /pool view</li> 
+                                    <li class="feature">Entertainment</li> 
+                                    <li class="feature">WIFI</li> 
+                                    <li class="feature">A/C</li> 
+                                    <li class="feature">King Bed</li> 
+                                    <li class="feature">Couch sofa with coffee table</li> 
+                                    <li class="feature">Television</li> 
                                 </div>
                                 <div class="col">
-                                    <li>Mini bar</li> 
-                                    <li>Dry kitchenette TCM Counter</li> 
-                                    <li>Shower</li> 
-                                    <li>Bathroom</li> 
-                                    <li>Bathrobe</li> 
-                                    <li>Bath linen</li> 
+                                    <li class="feature">Mini bar</li> 
+                                    <li class="feature">Dry kitchenette TCM Counter</li> 
+                                    <li class="feature">Shower</li> 
+                                    <li class="feature">Bathroom</li> 
+                                    <li class="feature">Bathrobe</li> 
+                                    <li class="feature">Bath linen</li> 
                                 </div>
-                            </div>
-                            
-                            </p> 
+                            </div>    
+                        </p> 
                 </div>
             </div>
         </div>
