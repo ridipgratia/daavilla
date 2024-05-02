5f7205202f2aafb3819c9d4ea8b22c53c1c609ce
dipu-kr <dipum906@gmail.com>
2024-05-02 13:42:19 +0530

    changing about img use asset

--- resources/views/components/about-content-component.blade.php
+++ resources/views/components/about-content-component.blade.php
@@ -1,11 +1,14 @@
 <div class="container-xxl py-5">
     <div class="container">
         <div class="row g-5 align-items-center">
-            
+
             <div class="col-lg-6">
                 <h6 class="text-start text-primary text-uppercase">About Us</h6>
                 <h1 class="mb-4">Welcome to <span class="text-primary text-uppercase">Hotel Daavilla</span></h1>
-                <p class="mb-4">Daavilla is in one of the trendiest sections of Guwahati, placing close to railway station, Airport, business district and popular attractions. The elegant Hotel “Daavilla’’ in Guwahati offers stellar amenities and a peaceful private location a short drive from heart of the city Dispur.</p>
+                <p class="mb-4">Daavilla is in one of the trendiest sections of Guwahati, placing close to railway
+                    station, Airport, business district and popular attractions. The elegant Hotel “Daavilla’’ in
+                    Guwahati offers stellar amenities and a peaceful private location a short drive from heart of the
+                    city Dispur.</p>
                 <details>
                     <summary class="text-primary">More...</summary>
                     <p class="mb-4">While staying at the hotel, you can cool off in our
@@ -14,7 +17,7 @@
                         over drinks or a quick bite with friends, head to the lounge Bar, open daily from noon until
                         11pm. Daavilla provides easy access to Lokpriya Gopinath Bordoloi International Airport
                         (GAU) 19km and the nearby railway station 10km. Nearest Hospital is in 1km.</p>
-                    
+
                     <p class="mb-4">Prefer just to relax? Lounge by our outdoor infinity pool and soak up scenic
                         private lawn. You can maintain your workout routine with a visit to our health club and
                         treat yourself to revitalizing treatments in our beautiful spa & parlour. With on-site
@@ -32,16 +35,17 @@
             <div class="col-lg-6">
                 <div class="row g-3">
                     <div class="col-6 text-end">
-                        <img class="img-fluid rounded w-75 zoomIn" src="img/child.jpg"  style="margin-top: 25%;">
+                        <img class="img-fluid rounded w-75 zoomIn" src="{{ asset('img/child.jpg') }}"
+                            style="margin-top: 25%;">
                     </div>
                     <div class="col-6 text-start">
-                        <img class="img-fluid rounded w-100 zoomIn" src="img/man.jpg">
+                        <img class="img-fluid rounded w-100 zoomIn" src="{{ asset('img/man.jpg') }}">
                     </div>
                     <div class="col-6 text-end">
-                        <img class="img-fluid rounded w-50 zoomIn" src="img/drinks.jpg">
+                        <img class="img-fluid rounded w-50 zoomIn" src="{{ asset('img/drinks.jpg') }}">
                     </div>
                     <div class="col-6 text-start">
-                        <img class="img-fluid rounded w-75 zoomIn" src="img/biryani.jpg">
+                        <img class="img-fluid rounded w-75 zoomIn" src="{{ asset('img/biryani.jpg') }}">
                     </div>
                 </div>
             </div>
@@ -51,62 +55,87 @@
             <div class="col-lg-6">
                 <div class="row g-3">
                     <div class="col-6 text-end">
-                        <img class="img-fluid rounded w-75 zoomIn" src="img/Kamakhya_Temple_Guwahati.JPG"  style="margin-top: 25%;">
+                        <img class="img-fluid rounded w-75 zoomIn" src="{{ asset('img/Kamakhya_Temple_Guwahati.jpg') }}"
+                            style="margin-top: 25%;">
                     </div>
                     <div class="col-6 text-start">
-                        <img class="img-fluid rounded w-100 zoomIn" src="img/Gatway_of_Kalakhetra,_Guwahati,_Assam.jpg">
+                        <img class="img-fluid rounded w-100 zoomIn"
+                            src="{{ asset('img/Gatway_of_Kalakhetra,_Guwahati,_Assam.jpg') }}">
                     </div>
                     <div class="col-6 text-end">
-                        <img class="img-fluid rounded w-50 zoomIn" src="img/Chital_or_Spotted_Deer.jpg">
+                        <img class="img-fluid rounded w-50 zoomIn" src="{{ assEt('img/Chital_or_Spotted_Deer.jpg') }}">
                     </div>
                     <div class="col-6 text-start">
-                        <img class="img-fluid rounded w-75 zoomIn" src="img/deepor.jpg">
+                        <img class="img-fluid rounded w-75 zoomIn" src="{{ asset('img/deepor.jpg') }}">
                     </div>
                 </div>
             </div>
             <div class="col-lg-6">
                 <h6 class=" text-start text-primary text-uppercase">Things to do in Guwahati</h6>
-                <p>Guwahati, the bustling capital city of Assam, India, offers a vibrant tapestry of cultural, historical, and natural attractions. Start your exploration with the Kamakhya Temple, a renowned Hindu pilgrimage site perched atop the Nilachal Hill. Delve into the region's rich history at the Assam State Museum, showcasing an impressive collection of artifacts and artworks. For a taste of local life, stroll through the bustling markets like Fancy Bazaar and Paltan Bazaar. Wildlife enthusiasts can visit the Assam State Zoo and Botanical Garden to see a variety of animal species, including the famous one-horned rhinoceros. Guwahati's culinary scene is also a highlight, with its array of authentic Assamese dishes and diverse culinary offerings.</p>
+                <p>Guwahati, the bustling capital city of Assam, India, offers a vibrant tapestry of cultural,
+                    historical, and natural attractions. Start your exploration with the Kamakhya Temple, a renowned
+                    Hindu pilgrimage site perched atop the Nilachal Hill. Delve into the region's rich history at the
+                    Assam State Museum, showcasing an impressive collection of artifacts and artworks. For a taste of
+                    local life, stroll through the bustling markets like Fancy Bazaar and Paltan Bazaar. Wildlife
+                    enthusiasts can visit the Assam State Zoo and Botanical Garden to see a variety of animal species,
+                    including the famous one-horned rhinoceros. Guwahati's culinary scene is also a highlight, with its
+                    array of authentic Assamese dishes and diverse culinary offerings.</p>
                 <details>
                     <summary class="text-primary">More...</summary>
                     <p>
-                        <h3 class="text-secondary">Kamakhya Temple</h3>
-                        20 mi / 13 km from the hotel <br>
-                        Kamakhya Temple in Guwahati is considered one of the oldest and most sacred centres of Tantric practices, dedicated to the goddess Kamakhya. Visit to learn about the history of the temple and to admire the surrounding Nilachal hills.
+                    <h3 class="text-secondary">Kamakhya Temple</h3>
+                    20 mi / 13 km from the hotel <br>
+                    Kamakhya Temple in Guwahati is considered one of the oldest and most sacred centres of Tantric
+                    practices, dedicated to the goddess Kamakhya. Visit to learn about the history of the temple and to
+                    admire the surrounding Nilachal hills.
                     </p>
                     <p>
-                        <h3 class="text-secondary">Assam state Zoo</h3>
-                        10 mi / 7 km from the hotel <br>
-                        Discover Deepor Beel Bird Sanctuary, a permanent freshwater lake located southwest of Guwahati city. It is renowned for its rich diversity of fauna and aquatic vegetation, which attracts wild elephants.
+                    <h3 class="text-secondary">Assam state Zoo</h3>
+                    10 mi / 7 km from the hotel <br>
+                    Discover Deepor Beel Bird Sanctuary, a permanent freshwater lake located southwest of Guwahati city.
+                    It is renowned for its rich diversity of fauna and aquatic vegetation, which attracts wild
+                    elephants.
                     </p>
                     <p>
-                        <h3 class="text-secondary"> Sankardev khalaketra </h3>
-                        10 mi / 5 km from the hotel <br>
-                        commonly known as Kalakshetra, is a cultural institution in the Panjabari area of Guwahati, Assam, India, named after the medieval poet-playwright and reformer Srimanta Sankardev. It includes a cultural museum, library, a children's park, and various facilities for preserving, demonstrating and performing cultural items and history. In addition to being Northeast India's largest cultural congregation, the Kalakshetra is also a major tourist spot in Guwahati
+                    <h3 class="text-secondary"> Sankardev khalaketra </h3>
+                    10 mi / 5 km from the hotel <br>
+                    commonly known as Kalakshetra, is a cultural institution in the Panjabari area of Guwahati, Assam,
+                    India, named after the medieval poet-playwright and reformer Srimanta Sankardev. It includes a
+                    cultural museum, library, a children's park, and various facilities for preserving, demonstrating
+                    and performing cultural items and history. In addition to being Northeast India's largest cultural
+                    congregation, the Kalakshetra is also a major tourist spot in Guwahati
                     </p>
                     <p>
-                        <h3 class="text-secondary">Guwahati University</h3>
-                        30 mi / 19 km from the hotel <br>
-                        One of northeast India’s oldest and most well-respected universities, Gauhati University (GU) was established in 1948 and is affiliated with more than 320 colleges and institutes throughout Lower Assam.
+                    <h3 class="text-secondary">Guwahati University</h3>
+                    30 mi / 19 km from the hotel <br>
+                    One of northeast India’s oldest and most well-respected universities, Gauhati University (GU) was
+                    established in 1948 and is affiliated with more than 320 colleges and institutes throughout Lower
+                    Assam.
                     </p>
                     <p>
-                        <h3 class="text-secondary">Deepor Beel</h3>
-                        25 mi / 17 km from the hotel <br>
-                        Discover Deepor Beel Bird Sanctuary, a permanent freshwater lake located southwest of Guwahati city. It is renowned for its rich diversity of fauna and aquatic vegetation, which attracts wild elephants.
+                    <h3 class="text-secondary">Deepor Beel</h3>
+                    25 mi / 17 km from the hotel <br>
+                    Discover Deepor Beel Bird Sanctuary, a permanent freshwater lake located southwest of Guwahati city.
+                    It is renowned for its rich diversity of fauna and aquatic vegetation, which attracts wild
+                    elephants.
                     </p>
-                    
-                    <p><h3 class="text-secondary">Lokpriya Gopinath Bordoloi International Airport <i class="fas fa-plane"></i></h3> <br>
-                        By car :
-                        Take the fastest route via NH-17 and arrive at the hotel in <span class="text-white p-1 fw-bold bg-warning rounded-1">45 minutes</span> <br>
+
+                    <p>
+                    <h3 class="text-secondary">Lokpriya Gopinath Bordoloi International Airport <i
+                            class="fas fa-plane"></i></h3> <br>
+                    By car :
+                    Take the fastest route via NH-17 and arrive at the hotel in <span
+                        class="text-white p-1 fw-bold bg-warning rounded-1">45 minutes</span> <br>
                     </p>
                     <p>
-                        <h3 class="text-secondary">Guwahati Railway Station <i class="fas fa-train"></i></h3> <br>
-                        By car :
-                        Take the fastest route via GS Road arrive at the hotel in about <span class="text-white p-1 fw-bold bg-warning rounded-1">20 minutes</span>
+                    <h3 class="text-secondary">Guwahati Railway Station <i class="fas fa-train"></i></h3> <br>
+                    By car :
+                    Take the fastest route via GS Road arrive at the hotel in about <span
+                        class="text-white p-1 fw-bold bg-warning rounded-1">20 minutes</span>
                     </p>
                 </details>
             </div>
-            
+
         </div>
     </div>
 </div>
