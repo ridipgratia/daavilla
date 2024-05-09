95aa53c0f5300cf1f16a4b13f324354924f206a0
ridipblog <memorytemp5@gmail.com>
2024-05-09 12:51:24 +0530

    add person room component

--- public/js/BookModule.js
+++ public/js/BookModule.js
@@ -1,7 +1,22 @@
-class BookModule{
-    constructor(){
-        this.start_date="";
-        this.end_date="";
+class BookModule {
+    constructor() {
+        this.start_date = "";
+        this.end_date = "";
+        this.name = "ridip";
+        this.ids = [];
+        this.ids['name'] = 1;
+        this.ids['name_1'] = 1;
+        this.room = {
+
+        }
+        this.pre_data = {
+
+        }
+        this.pre_data['adult'] = 1;
+        this.pre_data['child'] = 2;
+        this.room[this.name] = this.pre_data;
+        delete this.ids['name'];
+        console.log(this.ids);
     }
 }
 $(function () {
@@ -10,21 +25,21 @@ $(function () {
         locale: {
             cancelLabel: "Clear",
         },
-        parentEl: "#datePickerContainer",
+        // parentEl: "#datePickerContainer",
         // opens: 'center',
         // drops: "down",
     });
 
     $("#open-check-btn").on("apply.daterangepicker", function (ev, picker) {
-        this.start_date=picker.startDate.format("YYYY-MM-DD");
-        this.end_date=picker.endDate.format("YYYY-MM-DD");
-        var start_text_date=formatReadDate(this.start_date);
-        var end_text_date=formatReadDate(this.end_date);
+        this.start_date = picker.startDate.format("YYYY-MM-DD");
+        this.end_date = picker.endDate.format("YYYY-MM-DD");
+        var start_text_date = formatReadDate(this.start_date);
+        var end_text_date = formatReadDate(this.end_date);
         $('#check-in-out-date').html(`${start_text_date} - ${end_text_date}`);
         $(this).val(
             picker.startDate.format("MM/DD/YYYY") +
-                " - " +
-                picker.endDate.format("MM/DD/YYYY")
+            " - " +
+            picker.endDate.format("MM/DD/YYYY")
         );
     });
 
@@ -33,13 +48,18 @@ $(function () {
     });
 });
 // ------------------------ convert text format date -----------------
-function formatReadDate(date){
-    var options={
-        weekday:'short',
-        day:'numeric',
-        month:'short'
+function formatReadDate(date) {
+    var options = {
+        weekday: 'short',
+        day: 'numeric',
+        month: 'short'
     };
-    const new_date=new Date(date);
-    return  new_date.toLocaleDateString('en-US', options);
+    const new_date = new Date(date);
+    return new_date.toLocaleDateString('en-US', options);
 }
+
+// ------------------ open add room dislog -------------------
+$(document).on('click', '#open-room-dialog', function () {
+    $('.room-person-count-div').toggleClass('active-room-person-count-div');
+});
 export default BookModule;
--- resources/views/book_room.blade.php
+++ resources/views/book_room.blade.php
@@ -25,7 +25,8 @@
                         <p>Rooms & Person</p>
                         <h3>1 Room & 2Person</h3>
                     </div>
-                    <button type="button" class="col-1"><i class="fa-solid fa-caret-down"></i></button>
+                    <button type="button" class="col-1" id="open-room-dialog"><i
+                            class="fa-solid fa-caret-down"></i></button>
                 </div>
             </div>
             <input type="text" name="datefilter" value="" id="date_input" style="display: none;" />
@@ -33,74 +34,97 @@
 
             </div>
             <div class="d-flex flex-wrap col-12 room-person-count-div">
-                <div class="d-flex flex-wrap col-md-7 col-11 room-person-count-div-1">
-                    <div class="d-flex flex-wrap col-12 room-person-count-head">
-                        <div class="d-flex flex-wrap room-person-count-head-1">
-                            <span>Room</span>
-                            <span>1</span>
-                        </div>
-                        <button><i class="fa-solid fa-trash"></i></button>
-                    </div>
-                    <div class="d-flex flex-wrap col-12 room-person-count-content">
-                        <div class="d-flex flex-wrap col-12 room-person-count-content-1">
-                            <p class="col-md-5 col-12">Number Of Adults</p>
-                            <div class="d-flex flex-wrap col-md-5 col-12">
-                                <button><i class="fa-solid fa-minus"></i></button>
-                                <p id="number-of-adult"></p>
-                                <button><i class="fa-solid fa-plus"></i></button>
+                <div class="d-flex flex-wrap col-md-7 col-11 room-person-count-scroll-div">
+                    <div class="d-flex flex-wrap col-12 room-person-count-div-1 room-person-radius" id="">
+                        <div class="d-flex flex-wrap col-12 room-person-count-head">
+                            <div class="d-flex flex-wrap room-person-count-head-1">
+                                <span>Room</span>
+                                <span>1</span>
                             </div>
+                            <button><i class="fa-solid fa-trash"></i></button>
                         </div>
-                        <div class="d-flex flex-wrap col-12 room-person-count-content-1">
-                            <p class="col-md-5 col-12">Number Of Child</p>
-                            <div class="d-flex flex-wrap col-md-5 col-12">
-                                <button><i class="fa-solid fa-minus"></i></button>
-                                <p id="number-of-child"></p>
-                                <button><i class="fa-solid fa-plus"></i></button>
+                        <div class="d-flex flex-wrap col-12 room-person-count-content">
+                            <div class="d-flex flex-wrap col-11 room-person-count-content-1">
+                                <p class="col-md-5 col-12">Number Of Adults</p>
+                                <div class="d-flex flex-wrap col-md-5 col-12 room-person-count-content-2">
+                                    <button><i class="fa-solid fa-minus"></i></button>
+                                    <p id="number-of-adult">2</p>
+                                    <input type="number" style="display: none;">
+                                    <button><i class="fa-solid fa-plus"></i></button>
+                                </div>
+                            </div>
+                            <div class="d-flex flex-wrap col-11 room-person-count-content-1">
+                                <p class="col-md-5 col-12">Number Of Child</p>
+                                <div class="d-flex flex-wrap col-md-5 col-12 room-person-count-content-2">
+                                    <button><i class="fa-solid fa-minus"></i></button>
+                                    <p id="number-of-child">2</p>
+                                    <input type="number" style="display: none;">
+                                    <button><i class="fa-solid fa-plus"></i></button>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
+                <div class="d-flex flex-wrap col-md-7 col-11 room-person-add-div">
+                    <button><span><i class="fa-solid fa-plus"></i></span> <span>New Room</span></button>
+                    <button><span><i class="fa-solid fa-check"></i></span> <span>Done</span></button>
+                </div>
+            </div>
+            <div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias voluptate tenetur quibusdam
+                    exercitationem
+                    provident illum nulla quod aliquid iste nihil! Nisi itaque in distinctio possimus tempora cum? Dicta
+                    possimus ipsam unde repudiandae doloribus similique amet repellat iste rerum dolorum. Eveniet magnam
+                    molestias architecto natus nobis amet. Veritatis, quis quia. Natus ducimus, asperiores sapiente voluptas
+                    voluptates ex quo veniam repellendus. Porro officia iste, nisi vel nihil numquam similique consequuntur
+                    sed
+                    libero tempora, tenetur quo neque nemo rerum explicabo delectus perspiciatis praesentium illo asperiores
+                    quod laborum deserunt quae qui? Voluptatibus nostrum nemo nulla beatae, in aliquid dolorum fugiat
+                    praesentium ut a voluptatum recusandae dicta ipsum fugit facere quidem quis iure? Architecto dicta magni
+                    labore perspiciatis, dolorem fugiat recusandae natus itaque nisi vel dolor deleniti tenetur. Nam quae
+                    alias
+                    amet numquam incidunt, similique sint fuga omnis enim dolore, nulla quis ullam repudiandae. Facere rem
+                    laboriosam sint blanditiis? Sequi consequatur, nostrum debitis totam cupiditate velit? Dolorem
+                    perferendis
+                    assumenda quia animi fugit repellat, adipisci ab hic qui, culpa itaque reprehenderit porro ullam rem.
+                    Sed
+                    impedit modi nam tenetur necessitatibus nostrum tempore praesentium eius. Rem, sed veritatis? Distinctio
+                    obcaecati rerum quos consequatur officiis tempore nulla! Fugit adipisci, odio tempore voluptates et est
+                    amet, mollitia eaque nostrum consectetur atque explicabo vitae perferendis ad suscipit labore quidem
+                    dolorem
+                    tenetur harum possimus perspiciatis beatae? Vel commodi delectus necessitatibus quod praesentium culpa
+                    ducimus autem, officiis perspiciatis ea consequatur, nam expedita inventore temporibus veritatis ut rem
+                    natus soluta hic obcaecati dolores? Corrupti ea perspiciatis ducimus qui nesciunt deleniti possimus
+                    error
+                    voluptates a magni. Excepturi numquam doloremque, earum quisquam atque quia cupiditate dolores illum
+                    dolorem
+                    mollitia non blanditiis possimus repellendus molestiae, quaerat tempora eum quis suscipit! Veniam optio
+                    laboriosam voluptatum fugit ipsa ipsum dolor enim, nobis nesciunt quaerat? Adipisci, autem dolore.
+                    Accusantium ab eum illo repellat ipsa nemo atque, natus eveniet rerum voluptate dolore pariatur
+                    explicabo
+                    optio corrupti asperiores quaerat itaque delectus neque quibusdam amet nihil, nulla deserunt voluptatem!
+                    Ullam dolor officiis quae nulla ab id, nisi fuga doloremque doloribus, nesciunt eos, quis suscipit omnis
+                    vero iure temporibus porro dignissimos sit consequuntur placeat assumenda! Ullam similique adipisci,
+                    iste
+                    iure inventore, quod voluptas modi dolorem doloribus neque mollitia sit delectus illo aperiam provident
+                    tenetur est nulla facilis dolore! Nemo praesentium alias, porro at iusto minima ducimus magni molestiae
+                    nulla incidunt unde modi sequi amet voluptatem! Autem consequatur optio nesciunt mollitia. Voluptas
+                    facilis
+                    cum laboriosam vitae ipsum molestiae, veniam laborum fuga voluptates quam nostrum maxime delectus magnam
+                    temporibus assumenda sapiente ullam totam adipisci fugit repellat dignissimos doloremque nobis vel
+                    soluta.
+                    Quidem, incidunt eum. Ex iste unde suscipit iusto. Voluptatum possimus repudiandae, quod harum provident
+                    fugiat temporibus debitis dicta. Cupiditate nesciunt dolore iusto, sint quae error? Beatae quod natus
+                    voluptatem adipisci consectetur mollitia nobis necessitatibus sunt quisquam optio veniam amet minus
+                    suscipit
+                    tenetur, facere reiciendis! Consequatur, consectetur? Id doloribus temporibus dolorem totam corrupti
+                    deleniti. Porro atque deleniti ipsam nam laboriosam quo! Rem tempore quisquam perspiciatis suscipit
+                    similique fugit sequi autem impedit alias quasi deleniti cupiditate molestiae, fugiat ullam maiores
+                    mollitia
+                    voluptatibus officiis nisi ipsum eaque.</p>
             </div>
         </div>
-        <div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias voluptate tenetur quibusdam exercitationem
-                provident illum nulla quod aliquid iste nihil! Nisi itaque in distinctio possimus tempora cum? Dicta
-                possimus ipsam unde repudiandae doloribus similique amet repellat iste rerum dolorum. Eveniet magnam
-                molestias architecto natus nobis amet. Veritatis, quis quia. Natus ducimus, asperiores sapiente voluptas
-                voluptates ex quo veniam repellendus. Porro officia iste, nisi vel nihil numquam similique consequuntur sed
-                libero tempora, tenetur quo neque nemo rerum explicabo delectus perspiciatis praesentium illo asperiores
-                quod laborum deserunt quae qui? Voluptatibus nostrum nemo nulla beatae, in aliquid dolorum fugiat
-                praesentium ut a voluptatum recusandae dicta ipsum fugit facere quidem quis iure? Architecto dicta magni
-                labore perspiciatis, dolorem fugiat recusandae natus itaque nisi vel dolor deleniti tenetur. Nam quae alias
-                amet numquam incidunt, similique sint fuga omnis enim dolore, nulla quis ullam repudiandae. Facere rem
-                laboriosam sint blanditiis? Sequi consequatur, nostrum debitis totam cupiditate velit? Dolorem perferendis
-                assumenda quia animi fugit repellat, adipisci ab hic qui, culpa itaque reprehenderit porro ullam rem. Sed
-                impedit modi nam tenetur necessitatibus nostrum tempore praesentium eius. Rem, sed veritatis? Distinctio
-                obcaecati rerum quos consequatur officiis tempore nulla! Fugit adipisci, odio tempore voluptates et est
-                amet, mollitia eaque nostrum consectetur atque explicabo vitae perferendis ad suscipit labore quidem dolorem
-                tenetur harum possimus perspiciatis beatae? Vel commodi delectus necessitatibus quod praesentium culpa
-                ducimus autem, officiis perspiciatis ea consequatur, nam expedita inventore temporibus veritatis ut rem
-                natus soluta hic obcaecati dolores? Corrupti ea perspiciatis ducimus qui nesciunt deleniti possimus error
-                voluptates a magni. Excepturi numquam doloremque, earum quisquam atque quia cupiditate dolores illum dolorem
-                mollitia non blanditiis possimus repellendus molestiae, quaerat tempora eum quis suscipit! Veniam optio
-                laboriosam voluptatum fugit ipsa ipsum dolor enim, nobis nesciunt quaerat? Adipisci, autem dolore.
-                Accusantium ab eum illo repellat ipsa nemo atque, natus eveniet rerum voluptate dolore pariatur explicabo
-                optio corrupti asperiores quaerat itaque delectus neque quibusdam amet nihil, nulla deserunt voluptatem!
-                Ullam dolor officiis quae nulla ab id, nisi fuga doloremque doloribus, nesciunt eos, quis suscipit omnis
-                vero iure temporibus porro dignissimos sit consequuntur placeat assumenda! Ullam similique adipisci, iste
-                iure inventore, quod voluptas modi dolorem doloribus neque mollitia sit delectus illo aperiam provident
-                tenetur est nulla facilis dolore! Nemo praesentium alias, porro at iusto minima ducimus magni molestiae
-                nulla incidunt unde modi sequi amet voluptatem! Autem consequatur optio nesciunt mollitia. Voluptas facilis
-                cum laboriosam vitae ipsum molestiae, veniam laborum fuga voluptates quam nostrum maxime delectus magnam
-                temporibus assumenda sapiente ullam totam adipisci fugit repellat dignissimos doloremque nobis vel soluta.
-                Quidem, incidunt eum. Ex iste unde suscipit iusto. Voluptatum possimus repudiandae, quod harum provident
-                fugiat temporibus debitis dicta. Cupiditate nesciunt dolore iusto, sint quae error? Beatae quod natus
-                voluptatem adipisci consectetur mollitia nobis necessitatibus sunt quisquam optio veniam amet minus suscipit
-                tenetur, facere reiciendis! Consequatur, consectetur? Id doloribus temporibus dolorem totam corrupti
-                deleniti. Porro atque deleniti ipsam nam laboriosam quo! Rem tempore quisquam perspiciatis suscipit
-                similique fugit sequi autem impedit alias quasi deleniti cupiditate molestiae, fugiat ullam maiores mollitia
-                voluptatibus officiis nisi ipsum eaque.</p>
-        </div>
+
     </form>
 @endsection
 
