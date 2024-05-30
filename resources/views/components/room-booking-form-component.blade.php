1a0d0ed42f54e18e26db4868ddc7a8c669b93b1f
ridipblog <memorytemp5@gmail.com>
2024-05-30 11:37:23 +0530

    add somein g

--- resources/views/components/room-booking-form-component.blade.php
+++ resources/views/components/room-booking-form-component.blade.php
@@ -22,10 +22,6 @@
                         <p class="my-2">Ready to make unforgettable memories? Book now to reserve your comfortable
                             hotel room and
                             ensure a delightful stay!
-                        <div class="bg-light text-dark m"><span>Check-in 2:00 PM | Check-out
-                                12:00
-                                PM</span>
-                        </div>
                         </p>
                     </div>
                     <div class="d-flex justify-content-center align-items-center">
